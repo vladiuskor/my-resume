c6d79a01483a9a414ff33a7b8abd4d78efa7a7c2
vladko <max.vlad.kor@gmail.com>
2022-05-31 21:32:20 +0300

    Added template engine and enabled it

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+const path = require('path');
+const hbs = require('hbs');
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'templates/views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
-    res.send('Hello world!');
+    res.status(200).render('index');
 })
 
 app.listen(PORT, () => {
